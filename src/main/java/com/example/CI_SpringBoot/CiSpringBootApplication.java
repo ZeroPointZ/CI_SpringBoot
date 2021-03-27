ac376c554d9e938b1b3cbca7e07b1e8c9c8c77f0
student <mikedorado@gmail.com>
2021-03-27 16:43:35 +0000

    spring boot

---
+++ src/main/java/com/example/CI_SpringBoot/CiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.CI_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CI_SpringBoot/CiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CI_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
